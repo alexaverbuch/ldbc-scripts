fdeff2fa53f02f448dae17ffa7d0b231cb587563
Alex Averbuch <alex.averbuch@gmail.com>
2015-12-22 11:39:37 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Scripts to ease maintenance of LDBC repositories
+
+   * updating validation sets
+   * generating datasets
+   * keeping validation sets and connector repositories in sync
+   * etc.
---
+++ regenerate_test_dataset_and_copy_to_neo4j_connector.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Create new regular & merge versions of dataset for use in validation and integration tests. 
+# Copy datasets to Neo4j LDBC connector resources directory
+data_gen_dir=/Users/alexaverbuch/IdeaProjects/ldbc_snb_datagen
+scripts_dir=${data_gen_dir}/scripts
+
+rm ${data_gen_dir}/params.ini
+cp ${scripts_dir}/params_regular.ini ${data_gen_dir}/params.ini
+bash ${data_gen_dir}/run.sh
+bash ${scripts_dir}/copy_data_and_params_to_validation_set.sh 
+rm ${data_gen_dir}/params.ini
+cp ${scripts_dir}/params_merge.ini ${data_gen_dir}/params.ini
+bash ${data_gen_dir}/run.sh
+bash ${scripts_dir}/copy_merge_data_to_validation_set.sh
+rm ${data_gen_dir}/params.ini
+cp ${scripts_dir}/params_regular.ini ${data_gen_dir}/params.ini
